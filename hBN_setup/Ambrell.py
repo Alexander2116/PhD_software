f3c82027451c8a449ddc5b520e28086458c577e4
Aleksander Kedziora <aleksander2116k@gmail.com>
2024-03-19 17:36:34 +0000

    update

--- hBN_setup/Ambrell.py
+++ hBN_setup/Ambrell.py
@@ -6,20 +6,36 @@
 
 import serial
 import time
+import pyvisa
+import pymodbus.client as ModbusClient
+from pymodbus import (
+    ExceptionResponse,
+    Framer,
+    ModbusException,
+    pymodbus_apply_logging_config,
+)
 
 class Ambrell:
     def __init__(self, com:str, baud:int=38400):
         self._com:str = com
-        self._instr = serial.Serial(com,baud) # instrument
+        self._instr = serial.Serial(com,baud,xonxoff=False) # instrument
         self.var_max_amps = 0
-        self.__max_amps()
         
-    def __send_comm(self,comm:str, arg:str=None):
+    def __send_comm(self,comm:str, arg:str=None, encode:str='ascii', terminating_ch="", start_ch = "1,"):
         if(arg is not None):
-            command =  "1," + comm + "," + arg # self._com.replace("COM","")
+            command =  start_ch + comm + "," + str(arg) + terminating_ch # self._com.replace("COM","")
         else:
-            command = "1," + comm 
-        self._instr.write(command.encode('utf-8'))
+            command = start_ch + comm + terminating_ch
+        self._instr.write(command.encode(encode) )
+        self._instr.flush()
+    
+    def send_comm(self,comm:str, arg:str=None, encode:str='ascii', terminating_ch="", start_ch = "1,"):
+        if(arg is not None):
+            command =  start_ch + comm + "," + str(arg) + terminating_ch # self._com.replace("COM","")
+        else:
+            command = start_ch + comm + terminating_ch
+        print(command)
+        self._instr.write(command.encode(encode) )
     
     def __read(self) -> str:
         l = 0
@@ -139,3 +155,158 @@ class Ambrellv2:
         
     def timer(self, timer:float):
         self.__send_comm("timer",timer)
+        
+        
+class Ambrellv3:
+    def __init__(self, com:str, baud:int=38400):
+        self._com:str = com
+
+        self._instr = ModbusClient.ModbusSerialClient(
+            method ='ascii',
+            port = com,
+            framer=Framer.SOCKET,
+            # timeout=10,
+            # retries=3,
+            # retry_on_empty=False,
+            # strict=True,
+            baudrate=baud,
+            bytesize=8,
+            parity="N",
+            stopbits=1,
+            # handle_local_echo=False,
+        )
+        self.var_max_amps = 0
+        self._instr.connect()
+        
+    def __send_comm(self,comm:str, arg:str=None):
+        if(arg is not None):
+            command =  "1," + comm + "," + str(arg) + "\r\n" # self._com.replace("COM","")
+        else:
+            command = "1," + comm + "\r\n"
+        self._instr.send(command.encode('utf-8') )
+    
+    def __read(self) -> str:
+        l = 0
+        text:str = ""
+        while(l<4):
+            text=self._instr.read_coils(1)
+            l = len(text)
+        if("\r\n" not in str(text[0:len(text)].decode("utf-8"))):
+            return "0\r\n".encode("utf-8")
+        else:
+            return text
+        
+    def __max_amps(self):
+        self.__send_comm("maxamps")
+        self.var_max_amps = int(self.__read())
+    
+    @property
+    def max_amps(self) -> float:
+        return self.var_max_amps
+    
+    def help(self):
+        self.__send_comm("?")
+        print(self.__read())
+        
+    def header(self):
+        # Shows CHDR and Ctime
+        # CHDR (pronounced "Cheddar", like the cheese) or "Compressed Header" packets
+        # Ctime is the time the file's inode was last changed, while mtime is the last time the file's contents were changed
+        self.__send_comm("c?")
+        print(self.__read())
+    
+    def rtemp(self):
+        # raw temperature
+        self.__send_comm("rtemp")
+        return self.__read()
+    
+    def temp(self):
+        # raw temperature
+        self.__send_comm("temp")
+        return self.__read()
+    
+    def set_amps(self, value:float) -> None:
+        # can be set from 0 to max_amps
+        self.__send_comm("amps", value)
+        
+    def stat(self):
+        self.__send_comm("stat")
+        print(self.__read())
+        
+    def timer(self, timer:float):
+        self.__send_comm("timer",timer)
+        
+class Ambrellv4:
+    def __init__(self, com:str, baud:int=38400):
+        self._com:str = com
+        res = pyvisa.ResourceManager()
+        self._instr = res.open_resource(com) #serial.Serial(com,baud,xonxoff=True) # instrument
+        self.var_max_amps = 0
+        
+    def __send_comm(self,comm:str, arg:str=None, encode:str='ascii', terminating_ch="", start_ch = "1,"):
+        if(arg is not None):
+            command =  start_ch + comm + "," + str(arg) + terminating_ch # self._com.replace("COM","")
+        else:
+            command = start_ch + comm + terminating_ch
+        self._instr.write(command.encode(encode) )
+        self._instr.flush()
+    
+    def send_comm(self,comm:str, arg:str=None, encode:str='ascii', terminating_ch="", start_ch = "1,"):
+        if(arg is not None):
+            command =  start_ch + comm + "," + str(arg) + terminating_ch # self._com.replace("COM","")
+        else:
+            command = start_ch + comm + terminating_ch
+        print(command)
+        self._instr.write(command.encode(encode) )
+    
+    def __read(self) -> str:
+        l = 0
+        text:str = ""
+        while(l<4):
+            text=self._instr.read_all()
+            l = len(text)
+        if("\r\n" not in str(text[0:len(text)].decode("utf-8"))):
+            return "0\r\n".encode("utf-8")
+        else:
+            return text
+        
+    def __max_amps(self):
+        self.__send_comm("maxamps")
+        self.var_max_amps = int(self.__read())
+    
+    @property
+    def max_amps(self) -> float:
+        return self.var_max_amps
+    
+    def help(self):
+        self.__send_comm("?")
+        print(self.__read())
+        
+    def header(self):
+        # Shows CHDR and Ctime
+        # CHDR (pronounced "Cheddar", like the cheese) or "Compressed Header" packets
+        # Ctime is the time the file's inode was last changed, while mtime is the last time the file's contents were changed
+        self.__send_comm("c?")
+        print(self.__read())
+    
+    def rtemp(self):
+        # raw temperature
+        self.__send_comm("rtemp")
+        return self.__read()
+    
+    def temp(self):
+        # raw temperature
+        self.__send_comm("temp")
+        return self.__read()
+    
+    def set_amps(self, value:float) -> None:
+        # can be set from 0 to max_amps
+        self.__send_comm("amps", value)
+        
+    def stat(self):
+        self.__send_comm("stat")
+        print(self.__read())
+        
+    def timer(self, timer:float):
+        self.__send_comm("timer",timer)
+        
--- hBN_setup/test.py
+++ hBN_setup/test.py
@@ -1,4 +1,4 @@
-from Ambrell import Ambrellv2
+from Ambrell import Ambrell
 from time import sleep
 
 import serial.tools.list_ports
@@ -8,7 +8,22 @@ for port, desc, hwid in sorted(ports):
         print("{}: {} [{}]".format(port, desc, hwid))
 
 
-dev = Ambrellv2("COM8","COM9")
+
+# Commands
+starting_char = ["","1,"]
+terminating = ["","\r\n","\r","\n"]
+comm = ["amps,10","AMPS,10","Amps,10","amp,10.0"]
+encode = ["ascii",'utf-8','latin','utf-7','utf-16']
+
+
+dev = Ambrell("COM9")
+
+for i in starting_char:
+        for j in terminating:
+                for k in comm:
+                        for l in encode:
+                                dev.send_comm(k,encode=l,terminating_ch=j,start_ch=i)
+                                sleep(0.25)
 
 #dev.header() # print header
 sleep(1)
